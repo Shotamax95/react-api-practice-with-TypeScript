d28e733e108c63696fee89c7f87140731471a4ee
Shota Ito <shota.110.free.style@gmail.com>
2021-08-31 17:03:50 -0400

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,8 @@
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[Done]" : "[Not Yet]";
+  return <p>{`${completeMark} : ${title}(User: ${userId})`}</p>;
+};
